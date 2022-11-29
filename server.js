cfe7d5420454a086f7822283c2b8904cf37b82ed
MadisonSawvel <msawvel@nmu.edu>
2022-11-29 14:21:30 -0500

    server resposes being updated, index update

--- server.js
+++ server.js
@@ -3,24 +3,34 @@ var server = express();
 bodyParser = require("body-parser");
 server.use(bodyParser.urlencoded({extended: true}));
 
-let highscores = {};
+let highScores = {};
 
 server.post("/addNewScore", function(req,res){
-    
+    if(!highScores[req.body.user]) highScores[req.body.user] = 0;
+    if(highScores.length <= 5){
+        highScores[req.body.user]++;
+    }
+    //after, check if new score is higher than the current 5
+    //if yes, replace 
+    //if no, "sorry, try again"
     res.end();
 });
 
 server.get("/topFiveScores", function (req, res) {
     res.set("Content-Type", "text/plain");
     res.set("Cache-Control", "no-cache");
-    
+    //are we doing a cache?
+    for(let topScores in highScores){
+        res.write(req.query.topScores + " has " + highScores[req.body.topScores] + "points \n");
+    }
+    //way to get points in max to min order
     res.end();
 });
 
 server.get("/highestScore", function(req,res){
     res.set("Content-Type", "text/plain");
     res.set("Cache-Control", "no-cache");
-
+    //check for max
     res.end();
 });
 
