9fae6f730a73f11d2c167b0ba2a91ad17345f1ae
AlexDrent <74375353+AlexDrent@users.noreply.github.com>
2022-11-30 09:45:57 -0500

    better keystorke funtion

--- pub/Client.js
+++ pub/Client.js
@@ -7,19 +7,30 @@ function randomColor() {
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
+    keyMapper();
+});
+
+function keyMapper() {
     let buffer = [];
+    let lastKeyTime = Date.now();
 
     document.addEventListener('keydown', event => {
         const charList = 'abcdefghijklmnopqrstuvwxyz0123456789←→↓↑';
         const key = event.key.toLowerCase();
 
-        // we are only interested in alphanumeric keys
-        if (charList.indexOf(key) === -1) return;
+
+        const currentTime = Date.now();
+
+        if (currentTime - lastKeyTime > 1000) {
+            buffer = [];
+        }
 
         buffer.push(key);
-        console.log(buffer);
+        lastKeyTime = currentTime;
+
+        console.log(buffer)
     });
-});
+}
 //---------------------------------------------------------------------------------
 
 let myApp = Vue.createApp({
