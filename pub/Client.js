a62541179605a271fac119b9b03441410f477c51
AlexDrent <74375353+AlexDrent@users.noreply.github.com>
2022-12-03 15:49:27 -0500

    nothing i did nothing

--- pub/Client.js
+++ pub/Client.js
@@ -1,5 +1,3 @@
-const { buffer } = require("stream/consumers");
-
 function randomColor() {
     let ret = "#";
     for (let i = 0; i < 6; i++) ret += Math.floor(Math.random() * 16).toString(16);
@@ -49,30 +47,37 @@ let myApp = Vue.createApp({
     },
 
     methods: {
-        addBgImage() {
-            const img = new Image();
-            img.src = "img/back.png";
-            img.onload = () => {
-                this.ctx.drawImage(img, 0, 0);
-            };
+        drawLine(x1, y1, x2, y2, color1, color2) {
+            //set up the line's gradient
+            let g = this.ctx.createLinearGradient(x1, y1, x2, y2);
+            g.addColorStop(0, color1);
+            g.addColorStop(1, color2);
+            this.ctx.strokeStyle = g;
 
+            //Draw the line
+            this.ctx.beginPath(); //clears out any previous draw paths.
+            this.ctx.lineWidth = 2;
+            this.ctx.moveTo(x1, y1); //moves your "pen" to that location
+            this.ctx.lineTo(x2, y2); //draws to that location
+            this.ctx.stroke(); //put ink down
         },
         redrawEverything() {
-            this.addBgImage();
-            
-            //For the lines! - KEEP for redraw reference
-                // for (let j = 0; j < this.linesPerNode; j++) { //draws "this.linesPerNode" lines to the next node
-                //     let n = this.nodes[i];
-                //     let m = this.nodes[i + 1];
-                //     this.drawLine(n.x + n.dx * j, n.y + n.dy * j, m.x + m.dx * j, m.y + m.dy * j, n.color, m.color);
-                // }
-
+            this.ctx.fillStyle = this.backColor;
+            this.ctx.fillRect(0, 0, 400, 400);
+            //For the lines!
+            for (let i = 0; i < this.nodes.length - 1; i++) { // -1 here because we look ahead to draw to the next node
+                for (let j = 0; j < this.linesPerNode; j++) { //draws "this.linesPerNode" lines to the next node
+                    let n = this.nodes[i];
+                    let m = this.nodes[i + 1];
+                    this.drawLine(n.x + n.dx * j, n.y + n.dy * j, m.x + m.dx * j, m.y + m.dy * j, n.color, m.color);
+                }
+            }
         },
-        addGorn() {
+        addNode() {
 
             this.redrawEverything();
         },
-        deleteGorn(indexToDelete) {
+        deleteNode(indexToDelete) {
 
             this.redrawEverything();
         },
@@ -87,11 +92,7 @@ let myApp = Vue.createApp({
         },
     },
 
-    computed: {
-        addToScore(){
-
-        }
-    },
+    computed: {},
 
     mounted() {
         this.ctx = this.$refs.splineDisplay.getContext("2d"); //gets the graphics context for drawing
