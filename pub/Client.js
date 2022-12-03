28832795753943ae432bca488c25ef1348c65604
Caroline Christenson <carchris@nmu.edu>
2022-12-03 15:56:13 -0500

    ??

--- pub/Client.js
+++ pub/Client.js
@@ -18,6 +18,7 @@ function keyMapper() {
         const charList = 'abcdefghijklmnopqrstuvwxyz0123456789←→↓↑';
         const key = event.key.toLowerCase();
 
+
         const currentTime = Date.now();
 
         if (currentTime - lastKeyTime > 1000) {
@@ -28,7 +29,8 @@ function keyMapper() {
         lastKeyTime = currentTime;
 
 
-        console.log(buffer)
+        console.log(buffer);
+        return buffer[0];
     });
 }
 //---------------------------------------------------------------------------------
@@ -41,24 +43,12 @@ let myApp = Vue.createApp({
             linesPerNode: 20,
             ctx: null,
             nodes: [],
+            currentScore: 0,
+            highScore: null,
         };
     },
 
     methods: {
-        drawLine(x1, y1, x2, y2, color1, color2) {
-            //set up the line's gradient
-            let g = this.ctx.createLinearGradient(x1, y1, x2, y2);
-            g.addColorStop(0, color1);
-            g.addColorStop(1, color2);
-            this.ctx.strokeStyle = g;
-
-            //Draw the line
-            this.ctx.beginPath(); //clears out any previous draw paths.
-            this.ctx.lineWidth = 2;
-            this.ctx.moveTo(x1, y1); //moves your "pen" to that location
-            this.ctx.lineTo(x2, y2); //draws to that location
-            this.ctx.stroke(); //put ink down
-        },
         addBgImage() {
             const img = new Image();
             img.src = "img/back.png";
@@ -69,7 +59,6 @@ let myApp = Vue.createApp({
         },
         redrawEverything() {
             this.addBgImage();
-            this.gornPlacement();
             
             //For the lines! - KEEP for redraw reference
                 // for (let j = 0; j < this.linesPerNode; j++) { //draws "this.linesPerNode" lines to the next node
@@ -79,29 +68,36 @@ let myApp = Vue.createApp({
                 // }
 
         },
-        addNode() {
+        addGorn() {
 
             this.redrawEverything();
         },
-        deleteNode(indexToDelete) {
+        deleteGorn(indexToDelete) {
 
             this.redrawEverything();
         },
         movePlayer() {
-
+            if (buffer[0] == "→") {
+                //if all the way right dont move
+                //else move +100 px
+            } else if (buffer[0] == "←") {
+                //if all the way left dont move
+                //else move -100 px
+            }
         },
-        gornPlacement() {
-            const gImg = new Image();
-            gImg.src = "img/gorn.png";
-            gImg.onload = () => {
-                this.ctx.drawImage(gImg, 1, 1, 75, 75);
-            };
-        }
     },
 
     computed: {
         addToScore(){
-
+            currentScore += 1701;
+            return currentScore;
+        },
+        highScore(){
+            if (this.currentScore > this.highScore){
+                return this.currentScore;
+            }
+            return this.highScore;
+            }
         }
     },
 
